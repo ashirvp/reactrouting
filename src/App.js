17010891694562b5eeea218c72486b73d3ad3f65
Ashir Vp <54493280+ashirvp@users.noreply.github.com>
2021-11-13 10:15:59 +0530

    Commit added

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import { useState } from "react";
 import About from "./container/About";
 import Profile from "./container/Profile";
 
-
 function App() {
 
   const [state, setState] = useState('')
-  let componet
-  
-
-
-  
+  let component
+  if (state === 'Profile')
+    component = <Profile />
+  else if (state === 'About')
+    component = <About />
+    
   return (
     <div className="App">
+      
       <button onClick={() => setState('Profile')}>Profile</button>
       <button onClick={() => setState('About')}>About</button>
-      {componet}
+      {component}
+      
     </div>
   );
 }
